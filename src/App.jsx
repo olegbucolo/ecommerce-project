211e2ba1134e490208c41718ae150c8dbc5d7184
olegbucolo <olegbucolo97@gmail.com>
2025-12-20 19:27:20 +0100

    refactor(orders, tracking): added order and tracking elements to the react app and also added their routes to the router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { Orders } from './pages/Orders'
+import { Tracking } from './pages/Tracking'
 import './App.css'
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage/>} />
+      <Route path="orders" element={<Orders/>} />
+      <Route path="tracking" element={<Tracking/>} />
     </Routes>
   )
 }
