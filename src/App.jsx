1c2f1ebe643d26d5ce4e0bb284f9ec85b5430984
olegbucolo <olegbucolo97@gmail.com>
2025-12-23 16:20:46 +0100

    Lift the card up and share it between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { Orders } from './pages/Orders'
@@ -6,12 +8,20 @@ import { Tracking } from './pages/Tracking'
 import './App.css'
 
 function App() {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => {
+        setCart(response.data)
+      })
+
+  })
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage/>} />
-      <Route path="orders" element={<Orders/>} />
-      <Route path="tracking" element={<Tracking/>} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<Orders />} />
+      <Route path="tracking" element={<Tracking />} />
     </Routes>
   )
 }
