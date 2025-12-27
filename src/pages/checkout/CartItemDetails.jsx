6078ac074882ca5027bc0f2556a545bc42176481
olegbucolo <olegbucolo97@gmail.com>
2025-12-28 00:38:47 +0100

    Add functionality to the update button in the CheckoutPage under each item. Now updates items in the back.

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -1,12 +1,46 @@
 import axios from 'axios';
 import { formatMoney } from '../../utils/money';
 import { DeliveryOptions } from './DeliveryOptions';
+import { useState } from 'react';
 
 export function CartItemDetails({ cartItem, deliveryOptions, loadCart }) {
+
+    const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
+    const [quantity, setQuantity] = useState(cartItem.quantity);
+
     const deleteCartItem = async () => {
         await axios.delete(`/api/cart-items/${cartItem.productId}`)
         await loadCart();
     }
+
+    const updateQuantity = async () => {
+
+        if (isUpdatingQuantity) {
+            await axios.put(`/api/cart-items/${cartItem.productId}`, {
+                quantity: parseInt(quantity)
+            })
+            await loadCart();
+            setIsUpdatingQuantity(false);
+        } else {
+
+            setIsUpdatingQuantity(true);
+        }
+    }
+
+    const updateQuantityInputKey = (e) =>{
+        if(e.key === 'Enter'){
+            updateQuantity();
+        }else if(e.key === "Escape"){
+            setQuantity(cartItem.quantity);
+            setIsUpdatingQuantity(false);
+        }
+    }
+
+
+    const updateQuantityInput = (event) => {
+        setQuantity(event.target.value);
+    }
+
     return (
         <div className="cart-item-details-grid">
             <img className="product-image"
@@ -21,19 +55,31 @@ export function CartItemDetails({ cartItem, deliveryOptions, loadCart }) {
                 </div>
                 <div className="product-quantity">
                     <span>
-                        Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+                        Quantity:
+                        {
+                            isUpdatingQuantity
+                                ? <input
+                                    className="product-quantity-update"
+                                    type="text"
+                                    value={quantity}
+                                    onChange={updateQuantityInput} 
+                                    onKeyDown={updateQuantityInputKey}/>
+                                : <span className="quantity-label">{cartItem.quantity}</span>
+                        }
                     </span>
-                    <span className="update-quantity-link link-primary">
+                    <span className="update-quantity-link link-primary"
+                        onClick={updateQuantity}
+                    >
                         Update
                     </span>
                     <span className="delete-quantity-link link-primary"
-                    onClick={deleteCartItem}>
+                        onClick={deleteCartItem}>
                         Delete
                     </span>
                 </div>
             </div>
 
-            <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart}/>
+            <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
         </div>
     )
 }
