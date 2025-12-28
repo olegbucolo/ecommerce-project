0b45c93ef6942ee92878824e94836501fb98621f
olegbucolo <olegbucolo97@gmail.com>
2025-12-28 01:22:24 +0100

    Add Search feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,28 @@
 import './Header.css'
-import { NavLink } from 'react-router';
+import { NavLink, useNavigate, useSearchParams } from 'react-router';
+import { useState } from 'react';
 
 export function Header({ cart }) {
+    const navigate = useNavigate();
+
+    const [searchParams] = useSearchParams();
+    const searchText = searchParams.get('search');
+
+    const [search, setSearch] = useState(searchText || '');
     let totalQuantity = 0;
 
     cart.forEach((cartItem) => {
         totalQuantity += cartItem.quantity;
     })
+
+    const updateSearchInput = (event) => {
+        setSearch(event.target.value);
+    }
+
+    const searchProducts = () => {
+        navigate(`/?search=${search}`)
+        console.log(search);
+    }
 
     return (
         <div className="header">
@@ -20,9 +36,15 @@ export function Header({ cart }) {
             </div>
 
             <div className="middle-section">
-                <input className="search-bar" type="text" placeholder="Search" />
+                <input className="search-bar"
+                    type="text"
+                    placeholder="Search"
+                    onChange={updateSearchInput}
+                    value={search}
+                />
 
-                <button className="search-button">
+                <button className="search-button"
+                    onClick={searchProducts}>
                     <img className="search-icon" src="images/icons/search-icon.png" />
                 </button>
             </div>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,19 +2,26 @@ import { ProductsGrid } from './ProductsGrid'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
+import { useSearchParams } from 'react-router';
 import './HomePage.css';
 
 export function HomePage({ cart, loadCart }) {
     const [products, setProducts] = useState([]);
 
+    const [searchParams] = useSearchParams();
+    const search = searchParams.get('search');
+
     useEffect(() => {
-        const getHomeData = async () =>{
-            const response = await axios.get('/api/products')
+        const getHomeData = async () => {
+            const urlPath = search
+                ? `/api/products?search=${search}`
+                : '/api/products';
+            const response = await axios.get(urlPath)
             setProducts(response.data);
 
         }
         getHomeData();
-    }, [])
+    }, [search])
 
     return (
         <>
